34abace5e92caa836678763b7eaa91c3a2af119f
Neel <aidev.neel@gmail.com>
2023-03-23 15:31:13 +0530

    database change

--- demo_db.js
+++ demo_db.js
@@ -4,7 +4,7 @@ let config = {
     host    : 'localhost',
     user    : 'neel',
     password: 'home1234',
-    database: 'school'
+    database: 'home'
   };
 
 let conn = mysql.createConnection(config);
@@ -16,8 +16,9 @@ conn.connect(function(err) {
   
     console.log('Connected to the MySQL server.');
     console.log('Inserting Data.');
-    let sql = `INSERT INTO student(SName,Fav_book,BloodGroup) VALUES("Nodeuser","kaaaaak","js")`;
+    let sql = `INSERT INTO household(hhid, individ, access_code, username, name, email, phone, first_app_registration_on, recent_app_registration_on, is_pre_survey_completed, created_on, updated_on) VALUES ("test","ab123","xyz23","neel1234","neel","neel@gmail.com","5986598659","2023-01-23 12:45:56","2023-01-23 12:45:56",true,"2023-01-23 12:45:56","2023-01-23 12:45:56");`;
     conn.query(sql);
+    
 
     conn.end(function(err) {
         if (err) {
