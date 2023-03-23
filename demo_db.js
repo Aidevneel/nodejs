77c6999fa31c3c3948cada189e4bd3a4b89adf8e
Neel <aidev.neel@gmail.com>
2023-03-23 17:43:11 +0530

    insert update on duplicate using param(?)

--- demo_db.js
+++ demo_db.js
@@ -1,5 +1,18 @@
 let mysql = require('mysql');
 
+let hhid = "abc";
+let individ = "123";
+let access_code = "4asad0";
+let username  = "neel1234";
+let name = "meet";
+let email = "meet@yahoo.com";
+let phone = "5656565656";
+let first_reg = "2023-01-23 12:45:56";
+let recent_reg = "2023-01-23 12:45:56";
+let is_survey_completed = true;
+let created_on = "2023-01-23 12:45:56";
+let updated_on = "2023-01-23 12:11:11";
+
 let config = {
     host    : 'localhost',
     user    : 'neel',
@@ -16,9 +29,31 @@ conn.connect(function(err) {
   
     console.log('Connected to the MySQL server.');
     console.log('Inserting Data.');
-    let sql = `INSERT INTO household(hhid, individ, access_code, username, name, email, phone, first_app_registration_on, recent_app_registration_on, is_pre_survey_completed, created_on, updated_on) VALUES ("test","ab123","xyz23","neel1234","neel","neel@gmail.com","5986598659","2023-01-23 12:45:56","2023-01-23 12:45:56",true,"2023-01-23 12:45:56","2023-01-23 12:45:56");`;
-    conn.query(sql);
-    
+           
+    // conn.query(
+    //     'INSERT INTO household SET hhid = ?, individ = ?, access_code = ?, username = ?, name = ?, email = ?, phone = ?, first_app_registration_on = ?, recent_app_registration_on = ?, is_pre_survey_completed = ?, created_on = ?',
+    //     [ hhid, individ, access_code, username, name, email, phone, first_reg, recent_reg, is_survey_completed, created_on ], 
+    //     function (err, results) {
+    //         if (err) {
+    //             return console.log('error:' + err.message);
+    //           }
+    //           console.log('Insertion successful');
+    //     }
+    // );
+
+    conn.query(
+        `INSERT INTO household values(?,?,?,?,?,?,?,?,?,?,?,?) ON DUPLICATE KEY UPDATE 
+        access_code = "${access_code}", username = "${username}", name = "${name}", email = "${email}", 
+        phone = "${phone}", first_app_registration_on = "${first_reg}", recent_app_registration_on = "${recent_reg}", 
+        is_pre_survey_completed = ${is_survey_completed}, updated_on = "${updated_on}"`,
+        [ hhid, individ, access_code, username, name, email, phone, first_reg, recent_reg, is_survey_completed, created_on, updated_on ],
+        function (err, results) {
+            if (err) {
+                return console.log('error:' + err.message);
+              }
+              console.log('Insertion successful');
+        }
+    );
 
     conn.end(function(err) {
         if (err) {
@@ -27,6 +62,4 @@ conn.connect(function(err) {
         console.log('Close the database connection.');
       });
   });
-
-
   
